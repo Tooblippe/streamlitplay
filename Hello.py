9fe5634078330fbb28cfca53f564db7f3c3da110
tooblippe <tobie.nortje@gmail.com>
2022-06-08 16:00:21 +0200

    datafile?

--- Hello.py
+++ Hello.py
@@ -11,41 +11,42 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import numpy as np
+import pandas as pd
 import streamlit as st
 from streamlit.logger import get_logger
+import pandas
+from datetime import time
 
 LOGGER = get_logger(__name__)
 
 
 def run():
     st.set_page_config(
-        page_title="Hello",
+        page_title="Data Viewer",
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit Friends! 👋")
-    st.write(f"secret - {st.secrets['DB_NAME']}")
-    st.write(f"secret - {st.secrets['DB_SECRET']}")
-    st.sidebar.success("Select a demo above.")
+    st.write("# Garden Route Mall")
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+    cols = st.sidebar.multiselect('Plot', ['kW','kVA','kVAr'])
+    grouper = st.sidebar.selectbox('Agg Type', ['D','M','Y', 'H','M',])
+    month = st.sidebar.selectbox('month', [1,2,3,4,5,6,7,8,9,10,11,12])
+    df = pd.read_csv("data/garden_route_mall.csv", parse_dates={"DateTime":[0,1]})
+    df = df.set_index('DateTime')
+    df['H'] = df.index.hour
+    df['M'] = df.index.minute
+    df['D'] = df.index.day
+    df['M'] = df.index.month
+    df['Y'] = df.index.year
+
+    plt_df = df.loc[df.index.month==month].groupby(by=grouper).mean()[cols]
+
+    st.line_chart(plt_df)
+    st.write(plt_df)
+
+    for i in range(12):
+        st.line_chart(df.groupby(by='kW')['kW'].agg(Mean='mean', Max='max'))
 
 
 if __name__ == "__main__":
